c015f6c2da7fbbe8fdb5f562d36f0668687371fb
Sebastian Nette <sebastian-nette@gmx.de>
2015-03-24 13:30:18 +0100

    Update fnbind.js

--- fnbind.js
+++ fnbind.js
@@ -1,19 +1,52 @@
 Function.prototype.bind = (function()
 {
     // function stack
-    var fnStack = [];
+    var fnStack = [
+        function(fn,ctx) { return function() { fn.call(ctx); } },
+        function(fn,ctx) { return function(a) { fn.call(ctx,a); } },
+        function(fn,ctx) { return function(a,b) { fn.call(ctx,a,b); } },
+        function(fn,ctx) { return function(a,b,c) { fn.call(ctx,a,b,c); } },
+        function(fn,ctx) { return function(a,b,c,d) { fn.call(ctx,a,b,c,d); } },
+        function(fn,ctx) { return function(a,b,c,d,e) { fn.call(ctx,a,b,c,d,e); } },
+        function(fn,ctx) { return function(a,b,c,d,e,f) { fn.call(ctx,a,b,c,d,e,f); } }
+    ];
+    
+    var regexp = /,/g;
+    var countArgs = function (fn) {
+        var str = fn.toString();
+            str = str.substring(str.indexOf("(")+1, str.indexOf(")"));
+        return !str ? 0 : (1 + (str.length - str.replace(regexp, '').length));
+    };
+
+    // toString test
+    var test = (function() { try { return countArgs(function(a,b){}) === 2; } catch(e) { return false; } })();
+        
+    // original bind
+    var bind = Function.prototype.bind || function (context) {
+        var self = this;
+        return function () {
+            var args = Array.prototype.slice.call(arguments);
+            return self.apply(bind || null, args);
+        };
+    };
     
     // actual bind
-    return function( context )
+    return function(context)
     {
+        // if toString failed, use regular bind
+        if(!test)
+        {
+            return bind.call(this, context);
+        }
+        
         // count number of parameters
-        var n = this.toString().split(")")[0].split(",").length;
+        var n = countArgs(this);
         
         // create function in stack if not exist
-        if(fnStack[n] === undefined)
+        if(!fnStack[n] || typeof fnStack[n] !== 'function')
         {
             var args = [];
-            for( var i = 0; i < n; i++ )
+            for(var i = 0; i < n; i++)
             {
                 args.push("v"+i);
             }
