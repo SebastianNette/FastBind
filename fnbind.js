9ddcf305c85fb0dd75016c80a72a6cdcdbfc60f7
Sebastian Nette <sebastian-nette@gmx.de>
2015-03-25 08:16:14 +0100

    added arguments check

--- fnbind.js
+++ fnbind.js
@@ -4,14 +4,13 @@ Function.prototype.bind = (function()
     var fnStack = [];
     
     var regexp = /,/g;
-    var countArgs = function (fn) {
-        var str = fn.toString();
-            str = str.substring(str.indexOf("(")+1, str.indexOf(")"));
+    var countArgs = function (str) {
+        str = str.substring(str.indexOf("(")+1, str.indexOf(")"));
         return !str ? 0 : (1 + (str.length - str.replace(regexp, '').length));
     };
 
     // toString test
-    var test = (function () { try { return countArgs(function (a,b){}) === 2; } catch(e) { return false; } })();
+    var test = (function () { try { return countArgs((function (a,b){}).toString()) === 2; } catch(e) { return false; } })();
 
     // original bind
     var bind = Function.prototype.bind || function (ctx) {
@@ -37,9 +36,15 @@ Function.prototype.bind = (function()
         
         // if toString failed, use regular bind
         if(!test) return bind.apply(this, Array.prototype.slice.call(arguments));
+
+        // fn to string
+        var str = this.toString();
+
+        // more tests
+        var check_callee = (str.indexOf("native code") !== -1 || str.indexOf("arguments") !== -1);
         
         // count number of parameters
-        var n = countArgs(this);
+        var n = countArgs(str);
         var k = m - 1;
         
         // get fn for arguments length
@@ -59,14 +64,15 @@ Function.prototype.bind = (function()
             }
             
             // fn factory...
-            fnStack[k][n] = new Function("return function(f,c"+_args+"){return function("+_params.substring(1)+"){if(arguments.callee.length!==arguments.length){"+(_args?("var a=Array.prototype.slice.call(arguments); a.unshift("+_args.substring(1)+");return f.apply(c,a);"):"return f.apply(c,Array.prototype.slice.call(arguments));")+"}else{return f.call(c"+_args+_params+");}};};")();
+            fnStack[k][n] = new Function("return function(b,f,c"+_args+"){return function("+_params.substring(1)+"){if(b && arguments.callee.length!==arguments.length){"+(_args?("var a=Array.prototype.slice.call(arguments);a.unshift("+_args.substring(1)+");return f.apply(c,a);"):"return f.apply(c,Array.prototype.slice.call(arguments));")+"}else{return f.call(c"+_args+_params+");}};};")();
         }
                 
         // return bound fn
         if(args) {
             args.unshift(this);
+            args.unshift(check_callee);
             return fnStack[k][n].apply(null, args);
         }
-        return fnStack[k][n](this, ctx || null);
+        return fnStack[k][n](check_callee, this, ctx || null);
     };
 })();
